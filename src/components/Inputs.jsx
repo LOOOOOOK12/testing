db00b21e515fee6dcc93741273005652d39d229e
Luke Symon <lukealamil12@gmail.com>
2023-08-21 16:08:59 +0800

    update

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,14 @@
+import "./Inputs.css";
+
+const Inputs = () => {
+  return (
+    <>
+        <label>Username</label>
+        <input type="text" />
+        <label>Password</label>
+        <input type="password" />
+    </>
+  )
+}
+
+export default Inputs
